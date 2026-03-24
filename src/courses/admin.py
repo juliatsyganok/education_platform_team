e4fe97e1f2c124d208f1816eab5a9e75e5330b7c
juliatsyganok <julia.tsyganok.9@yandex.ru>
2026-03-24 14:50:36 +0300

    создана админка

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Course, Lesson, Enrollment, UserProfile
+admin.site.register(Course)
+admin.site.register(Lesson)
+admin.site.register(Enrollment)
+admin.site.register(UserProfile)
