5a2dfef307603d74f571bc0460b3c03dd1e5f13c
juliatsyganok <julia.tsyganok.9@yandex.ru>
2026-03-24 14:29:28 +0300

    Добавлены модели

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+    
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
+    name = models.CharField(max_length=200)
+    content = models.TextField()
+    order = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+class Enrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'course')
+
+    def __str__(self):
+        return f"{self.user.username} enrolled in {self.course.title}"
+    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile_images/', blank=True)
+
+    def __str__(self):
+        return self.user.username
