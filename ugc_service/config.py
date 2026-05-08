1c95baa6580fe9b76187e5d2b93be5c36c89da1c
juliatsyganok <julia.tsyganok.9@yandex.ru>
2026-05-08 15:21:49 +0300

    обновлены зависмости

--- ugc_service/config.py
+++ ugc_service/config.py
@@ -1,14 +1,13 @@
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+from dotenv import load_dotenv
+load_dotenv()
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'ugc.db')}"
-    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DJANGO_API_URL = os.getenv("DJANGO_API_URL", "http://localhost:8000")
     
-    DJANGO_API_URL = "http://localhost:8000"
-    
-    SECRET_KEY = "ugc-secret-key-change-in-production"
-
     DEBUG = True
